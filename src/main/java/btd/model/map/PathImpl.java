2023846e5c1759f690c97f223db6f9704e5c02c0
Riccardo Penazzi <riccardo.penazzi@studio.unibo.it>
2023-05-08 13:28:10 +0200

    Fixed PathImpl

--- src/main/java/btd/model/map/PathImpl.java
+++ src/main/java/btd/model/map/PathImpl.java
@@ -1,5 +1,9 @@
 package btd.model.map;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import btd.utils.Direction;
@@ -9,10 +13,12 @@ public class PathImpl implements Path{
     
     private List<Direction> path;
     private Position spawnPosition;
+	private String source;
 
-    public PathImpl(final List<Direction> path, final Position spawnPosition){
-        this.path = path;
-        this.spawnPosition = spawnPosition;
+    public PathImpl(final String source){
+        this.path = new ArrayList<>();
+		this.source = source;
+        loadPath(this.source);
     }
 
 	@Override
@@ -28,6 +34,43 @@ public class PathImpl implements Path{
 	@Override
 	public int getPathDistance() {
 		return this.path.size();
+	}
+
+	private void loadPath(final String source){
+		try {
+			InputStream input = getClass().getResourceAsStream(source);
+			BufferedReader br = new BufferedReader(new InputStreamReader(input));
+			String read = br.readLine();
+			String spawnPosition[] = read.split(" ");
+			this.spawnPosition.set(Double.parseDouble(spawnPosition[0]), Double.parseDouble(spawnPosition[1]));
+			int step = Integer.parseInt(spawnPosition[2]);
+			read = br.readLine();
+			String path[] = read.split(" ");
+			for(int i = 0; i < step; i++){
+				this.path.add(decodeDirection(Integer.parseInt(path[i])));
+			}
+			br.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private Direction decodeDirection(int in){
+		switch(in){
+			case 1: return Direction.UP;
+			case 2: return Direction.DOWN;
+			case 3: return Direction.RIGHT;
+			case 4: return Direction.LEFT;
+		}
+		return null; /*Da rivedere */
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((path == null) ? 0 : path.hashCode());
+		return result;
 	}
     
 }
