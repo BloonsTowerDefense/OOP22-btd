ca08871005d5bd0071d12dd7835e144bd9db89c2
penazziriccardo <penazziriccardo17@gmail.com>
2023-07-11 15:17:57 +0200

    fixed

--- src/main/java/btd/Main.java
+++ src/main/java/btd/Main.java
@@ -1,11 +1,23 @@
 //Batusha
 package btd;
 
-import btd.model.Game;
+import javax.swing.JFrame;
+
+import btd.model.map.MapPanel;
 
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+        JFrame frame = new JFrame("BTD");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        MapPanel mp = new MapPanel();
+       
+        frame.add(mp);
+        frame.pack();
+
+        frame.setVisible(true);
+        
+        mp.startGameThread();
     }
 }
