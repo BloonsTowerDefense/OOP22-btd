4882ebbdd386dea748b6cde15698b68955ddc1f2
Daniele Martignani <daniele.martignani@studio.unibo.it>
2023-08-03 18:26:34 +0200

    added GameCondition

---
+++ src/main/java/btd/view/GameCondition.java
@@ -0,0 +1,16 @@
+package btd.view;
+
+public enum GameCondition {
+    /**
+     * The game is running.
+     */
+    PLAY,
+    /**
+     * The game is finished.
+     */
+    OVER,
+    /**
+     * Main menu
+     */
+    MENU;
+}
