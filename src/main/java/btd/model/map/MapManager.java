fde18181a9365c694abe3460ccf65f01475b3a42
penazziriccardo <penazziriccardo17@gmail.com>
2023-08-04 18:24:41 +0200

    Added getBloonPath in MapManager

--- src/main/java/btd/model/map/MapManager.java
+++ src/main/java/btd/model/map/MapManager.java
@@ -3,5 +3,6 @@ import java.awt.Graphics2D;
 public interface MapManager {
     public void draw(Graphics2D graphics2d);
     public int[][] getMapNum();
+    public Path getBloonPath();
     //TODO getMap();
 }
