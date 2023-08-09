a6f06b384231c9cf51c000181f03c959b03eab63
StivenG <stiven.gjinaj@studio.unibo.it>
2023-08-09 18:42:43 +0200

    Fixed Bullet

--- src/main/java/btd/model/entity/Bullet.java
+++ src/main/java/btd/model/entity/Bullet.java
@@ -35,19 +35,29 @@ public class Bullet {
     public void updatePosition(double deltaTime, Graphics g) {
         double startX = startingPosition.getX();
         double startY = startingPosition.getY();
-        g.drawImage(this.sprite,(int) startX,(int) startY,null);
         double endX = targetPosition.getX();
         double endY = targetPosition.getY();
 
-        // Calculate the current position based on linear interpolation
-        double t = Math.min(1, this.elapsedTime/deltaTime);  // Ensure t doesn't exceed 1
+        double t = Math.min(1, this.elapsedTime / deltaTime);
+
         double currentX = startX + (endX - startX) * t;
         double currentY = startY + (endY - startY) * t;
 
-        // Update the bullet's position
-        setStartingPosition(new Position(currentX, currentY));
-        g.drawImage(this.sprite,(int) this.startingPosition.getX(),(int) this.startingPosition.getY(),null);
-        // Update the elapsed time
+        int trailCount = 4;      // Number of trail segments
+
+        // Draw the bullet trail behind the current position
+        for (int i = 0; i < trailCount; i++) {
+            double trailX = currentX - i * (endX - startX) * 0.3;
+            double trailY = currentY - i * (endY - startY) * 0.3;
+            g.drawImage(this.sprite, (int) trailX, (int) trailY, 20, 20, null);
+        }
+
+        // Draw the bullet at the current position
+        g.drawImage(this.sprite, (int) currentX, (int) currentY, 20, 20, null);
+
         this.elapsedTime += deltaTime;
     }
+
+
+
 }
