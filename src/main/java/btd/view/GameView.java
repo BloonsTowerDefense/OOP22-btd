16c7cf50c2b8edc20ce02d12fc3c6c243327f759
StivenG <stiven.gjinaj@studio.unibo.it>
2023-08-10 20:58:25 +0200

    Fixed checkstyle

--- src/main/java/btd/view/GameView.java
+++ src/main/java/btd/view/GameView.java
@@ -10,130 +10,156 @@ import btd.utils.SoundManager;
 import btd.view.menu.ShopMenu;
 import btd.view.menu.StatsMenu;
 import btd.view.menu.TowerUpgradeMenu;
-
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class GameView extends JPanel {
 
-    private final ShopMenu towerShopMenu;
-    private final MapPanel mapPanel;
+  private final ShopMenu towerShopMenu;
+  private final MapPanel mapPanel;
 
-    private final StatsMenu statsMenu;
+  private final StatsMenu statsMenu;
 
-    private final Game gameEngine;
+  private final Game gameEngine;
 
-    private String towerToPlace = "";
+  private String towerToPlace = "";
 
-    public GameView(Game gameEngine) {
-        this.gameEngine = gameEngine;
-        setLayout(new BorderLayout());
-        towerShopMenu = new ShopMenu();
-        mapPanel = new MapPanel(this.gameEngine);
-        statsMenu = new StatsMenu();
-        //gameEngine.getRankController().addScore(gameEngine.getGameModel().getMapManager().getMapName(), "Nome", 1028);
+  public GameView(Game gameEngine) {
+    this.gameEngine = gameEngine;
+    setLayout(new BorderLayout());
+    towerShopMenu = new ShopMenu();
+    mapPanel = new MapPanel(this.gameEngine);
+    statsMenu = new StatsMenu();
+    // gameEngine.getRankController().addScore(gameEngine.getGameModel().getMapManager().getMapName(), "Nome", 1028);
 
-        JPanel eastPanel = new JPanel();
-        eastPanel.setLayout(new BoxLayout(eastPanel,BoxLayout.PAGE_AXIS));
+    JPanel eastPanel = new JPanel();
+    eastPanel.setLayout(new BoxLayout(eastPanel, BoxLayout.PAGE_AXIS));
 
-        CardLayout cardLayout = new CardLayout();
-        JPanel shopUpgradePanel = new JPanel(cardLayout);
-        shopUpgradePanel.setPreferredSize(new Dimension(200, 620));
-        shopUpgradePanel.add(towerShopMenu, "SHOP");
+    CardLayout cardLayout = new CardLayout();
+    JPanel shopUpgradePanel = new JPanel(cardLayout);
+    shopUpgradePanel.setPreferredSize(new Dimension(200, 620));
+    shopUpgradePanel.add(towerShopMenu, "SHOP");
 
-        cardLayout.show(shopUpgradePanel, "SHOP");
+    cardLayout.show(shopUpgradePanel, "SHOP");
 
-        setupTowerButtons();
-        setupMapPanel(cardLayout, shopUpgradePanel);
+    setupTowerButtons();
+    setupMapPanel(cardLayout, shopUpgradePanel);
 
-        eastPanel.add(statsMenu);
-        eastPanel.add(shopUpgradePanel);
+    eastPanel.add(statsMenu);
+    eastPanel.add(shopUpgradePanel);
 
-        add(mapPanel, BorderLayout.CENTER);
-        add(eastPanel, BorderLayout.EAST);
-    }
+    add(mapPanel, BorderLayout.CENTER);
+    add(eastPanel, BorderLayout.EAST);
+  }
 
-    private void setupTowerButtons() {
-        towerShopMenu.getBlackAdam().addActionListener(e -> towerToPlace = "blackAdam");
-        towerShopMenu.getVoldelife().addActionListener(e -> towerToPlace = "voldelife");
-        towerShopMenu.getDeadColossus().addActionListener(e -> towerToPlace = "deadColossus");
-        towerShopMenu.getRangeEnhancer().addActionListener(e -> towerToPlace = "rangeEnhancer");
-        towerShopMenu.getPowerEnhancer().addActionListener(e -> towerToPlace = "powerEnhancer");
-    }
+  private void setupTowerButtons() {
+    towerShopMenu.getBlackAdam().addActionListener(e -> towerToPlace = "blackAdam");
+    towerShopMenu.getVoldelife().addActionListener(e -> towerToPlace = "voldelife");
+    towerShopMenu.getDeadColossus().addActionListener(e -> towerToPlace = "deadColossus");
+    towerShopMenu.getRangeEnhancer().addActionListener(e -> towerToPlace = "rangeEnhancer");
+    towerShopMenu.getPowerEnhancer().addActionListener(e -> towerToPlace = "powerEnhancer");
+  }
 
-    private void setupMapPanel(CardLayout cardLayout, JPanel shopUpgradePanel) {
-        mapPanel.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                
-                int spriteX = e.getX();
-                int spriteY = e.getY();
+  private void setupMapPanel(CardLayout cardLayout, JPanel shopUpgradePanel) {
+    mapPanel.addMouseListener(
+        new MouseAdapter() {
+          @Override
+          public void mouseClicked(MouseEvent e) {
 
-                Tower clickedTower = gameEngine.getGameModel().isTower(spriteX, spriteY);
+            int spriteX = e.getX();
+            int spriteY = e.getY();
 
-                if (clickedTower != null) {
-                    TowerUpgradeMenu upgradeMenu = new TowerUpgradeMenu(clickedTower);
-                    shopUpgradePanel.add(upgradeMenu, "UPGRADE");
-                    upgradeMenu.getUpgradeButton().addActionListener(event -> cardLayout.show(shopUpgradePanel, "SHOP"));
-                    cardLayout.show(shopUpgradePanel, "UPGRADE");
-                    JLabel upgradePrice = new JLabel(clickedTower.getUpgradePrice().toString()+" coins");
-                    upgradePrice.setFont(new Font("Arial",Font.BOLD,15));
-                    upgradePrice.setHorizontalAlignment(SwingConstants.CENTER);
-                    upgradeMenu.getUpgradeButton().add(BorderLayout.SOUTH,upgradePrice);
-                    upgradeMenu.getUpgradeButton().addActionListener(e1 -> {
-                        if(gameEngine.getGameModel().getPlayer().getCoins() >= clickedTower.getUpgradePrice()) {
-                            gameEngine.getGameModel().getPlayer().setCoins(gameEngine.getGameModel().getPlayer().getCoins()-clickedTower.getUpgradePrice());
-                            clickedTower.update();
-                            SoundManager.getInstance().playSound(SoundManager.SoundType.BUTTON, false);
+            Tower clickedTower = gameEngine.getGameModel().isTower(spriteX, spriteY);
+
+            if (clickedTower != null) {
+              TowerUpgradeMenu upgradeMenu = new TowerUpgradeMenu(clickedTower);
+              shopUpgradePanel.add(upgradeMenu, "UPGRADE");
+              upgradeMenu
+                  .getUpgradeButton()
+                  .addActionListener(event -> cardLayout.show(shopUpgradePanel, "SHOP"));
+              cardLayout.show(shopUpgradePanel, "UPGRADE");
+              JLabel upgradePrice =
+                  new JLabel(clickedTower.getUpgradePrice().toString() + " coins");
+              upgradePrice.setFont(new Font("Arial", Font.BOLD, 15));
+              upgradePrice.setHorizontalAlignment(SwingConstants.CENTER);
+              upgradeMenu.getUpgradeButton().add(BorderLayout.SOUTH, upgradePrice);
+              upgradeMenu
+                  .getUpgradeButton()
+                  .addActionListener(
+                      e1 -> {
+                        if (gameEngine.getGameModel().getPlayer().getCoins()
+                            >= clickedTower.getUpgradePrice()) {
+                          gameEngine
+                              .getGameModel()
+                              .getPlayer()
+                              .setCoins(
+                                  gameEngine.getGameModel().getPlayer().getCoins()
+                                      - clickedTower.getUpgradePrice());
+                          clickedTower.update();
+                          SoundManager.getInstance()
+                              .playSound(SoundManager.SoundType.BUTTON, false);
                         }
-                    });
-                }
-
-                if (mapPanel.getMapManager().canPlace(spriteX, spriteY) && !towerToPlace.isEmpty()) {
-                    //gameEngine.getRankController().addScore(gameEngine.getGameModel().getMapManager().getMapName(), "Nome", 1028);
-                    int newX = spriteX/48;
-                    int newY = spriteY/48;
-                    Tower tower = createTowerByType(newX*48, newY*48);
-                    if (tower != null) {
-                        mapPanel.getGameModel().addTower(tower);
-                    }
-                    towerToPlace = "";
-                }
+                      });
             }
-        });
-    }
 
-    private Tower createTowerByType(int spriteX, int spriteY) {
-        Tower tower = switch (towerToPlace) {
-            case "blackAdam" -> new ShootingTower("blackAdam", 1, 100, new Position(spriteX, spriteY));
-            case "voldelife" -> new ShootingTower("voldelife", 3, 200, new Position(spriteX, spriteY));
-            case "deadColossus" -> new ShootingTower("deadColossus", 5, 300, new Position(spriteX, spriteY));
-            case "rangeEnhancer" -> new HelpingTower("rangeEnhancer", "Range",100, new Position(spriteX, spriteY));
-            case "powerEnhancer" -> new HelpingTower("powerEnhancer", "Power", 200,new Position(spriteX, spriteY));
-            default -> null;
+            if (mapPanel.getMapManager().canPlace(spriteX, spriteY) && !towerToPlace.isEmpty()) {
+              // gameEngine.getRankController().addScore(gameEngine.getGameModel().getMapManager().getMapName(), "Nome", 1028);
+              int newX = spriteX / 48;
+              int newY = spriteY / 48;
+              Tower tower = createTowerByType(newX * 48, newY * 48);
+              if (tower != null) {
+                mapPanel.getGameModel().addTower(tower);
+              }
+              towerToPlace = "";
+            }
+          }
+        });
+  }
+
+  private Tower createTowerByType(int spriteX, int spriteY) {
+    Tower tower =
+        switch (towerToPlace) {
+          case "blackAdam" -> new ShootingTower(
+              "blackAdam", 1, 100, new Position(spriteX, spriteY));
+          case "voldelife" -> new ShootingTower(
+              "voldelife", 3, 200, new Position(spriteX, spriteY));
+          case "deadColossus" -> new ShootingTower(
+              "deadColossus", 5, 300, new Position(spriteX, spriteY));
+          case "rangeEnhancer" -> new HelpingTower(
+              "rangeEnhancer", "Range", 100, new Position(spriteX, spriteY));
+          case "powerEnhancer" -> new HelpingTower(
+              "powerEnhancer", "Power", 200, new Position(spriteX, spriteY));
+          default -> null;
         };
 
-        return this.statsMenu.getMoney() >= tower.getPrice() ? tower : null;
-    }
+    return this.statsMenu.getMoney() >= tower.getPrice() ? tower : null;
+  }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        mapPanel.paintComponent(g);
-        System.out.println("\n paint gameView");
-    }
+  @Override
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    mapPanel.paintComponent(g);
+    System.out.println("\n paint gameView");
+  }
 
-    public MapPanel getMapPanel() {
-        return mapPanel;
-    }
+  public MapPanel getMapPanel() {
+    return mapPanel;
+  }
 
-    public void setPlayerLife(String life){
-        this.statsMenu.setLifeLabel(life);
-    }
+  public void setPlayerLife(String life) {
+    this.statsMenu.setLifeLabel(life);
+  }
 
-    public void setPlayerMoney(String money){
-        this.statsMenu.setMoneyLabel(money);
-    }
+  public void setPlayerMoney(String money) {
+    this.statsMenu.setMoneyLabel(money);
+  }
 }
